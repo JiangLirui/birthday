027930b43cdea90081c958e28110e619753223ca
jianglirui <jiang_l_r@163.com>
2020-09-18 02:23:48 +0800

    修改按钮内容

--- config.js
+++ config.js
@@ -31,13 +31,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "看看是啥",
+        play: "点一点",
+        bannar_coming: "就这？",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "想说的话",
     }
 };
